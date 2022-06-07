0b9cae729e7df46e4c762e353a9dacdf25981929
甲斐聖健 <>
2022-06-07 16:15:39 +0900

    細部調整

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -23,11 +23,17 @@ $('.img_slider').slick({
     autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
     speed: 5000, //スライドが流れる速度を設定
     cssEase: "linear", //スライドの流れ方を等速に設定
-    slidesToShow: 3, //表示するスライドの数
+    slidesToShow: 5, //表示するスライドの数
     swipe: false, // 操作による切り替えはさせない
     arrows: false, //矢印非表示
     pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
     pauseOnHover: false,
+    responsive:[{
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 3,
+      }
+    }]
 });
 
 $('.serv01_slider').slick({
@@ -54,7 +60,7 @@ $('.title_slider').slick({
     autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
     speed: 5000, //スライドが流れる速度を設定
     cssEase: "linear", //スライドの流れ方を等速に設定
-    slidesToShow: 3, //表示するスライドの数
+    slidesToShow: 2, //表示するスライドの数
     swipe: false, // 操作による切り替えはさせない
     arrows: false, //矢印非表示
     pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
